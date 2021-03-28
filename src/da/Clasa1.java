2df69e54c1f8e2e00ac544c5b2f3f07dda1bcc58
RazvanF <razvan.fratiman@yahoo.com>
2021-03-28 21:50:46 +0300

    functie completa de afisare date

--- src/da/Clasa1.java
+++ src/da/Clasa1.java
@@ -4,8 +4,9 @@ public class Clasa1 {
 	int data[] = {2, 4, 8};
 	
 	public void display(){
+		System.out.println("Data: ");
 		for(int i = 0; i < data.length; i++){
-			System.out.println("");
+			System.out.println("Next value: "+data[i]);
 		}
 	}
 }
