fe975dbd9a49dad2ef4245d8b0684ac0e60fb640
RazvanF <razvan.fratiman@yahoo.com>
2021-03-28 19:32:09 +0300

    am adaugat prima schimbare

--- src/da/prima.java
+++ src/da/prima.java
@@ -5,6 +5,7 @@ public class prima {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test proiect");
+		System.out.println("Prima modificare locala");
 	}
 
 }
