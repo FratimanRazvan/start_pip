426669761d903293ae18f9e94f89a13aa154b8b8
RazvanF <razvan.fratiman@yahoo.com>
2021-03-28 21:33:06 +0300

    Implementare partiala pentru functie de afisare date

--- src/da/Clasa1.java
+++ src/da/Clasa1.java
@@ -4,6 +4,8 @@ public class Clasa1 {
 	int data[] = {2, 4, 8};
 	
 	public void display(){
-		System.out.println("To do");
+		for(int i = 0; i < data.length; i++){
+			System.out.println("");
+		}
 	}
 }
